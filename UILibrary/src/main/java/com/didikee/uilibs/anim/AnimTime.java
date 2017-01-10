1fb0131ab4aef526f3c162e13d0416a638337b9d
wwh <wg-dd@qq.com>
2017-01-10 16:31:43 +0800

    delete some no important

--- UILibrary/src/main/java/com/didikee/uilibs/anim/AnimTime.java
+++ UILibrary/src/main/java/com/didikee/uilibs/anim/AnimTime.java
@@ -3,11 +3,14 @@ package com.didikee.uilibs.anim;
 /**
  * Created by didik 
  * Created time 2017/1/10
- * Description: 
+ * Description: android 动画的时间,按照官方的指示定义的三个常用时间,第四个为temp.
  */
 
 public class AnimTime {
     public static final int LongDuration = 375;
     public static final int NormalDuration = 250;
     public static final int ShortDuration = 150;
+    public static final int LongShortDuration = 300;
+
+
 }
